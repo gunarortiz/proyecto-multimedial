cefe1499826b37e86a67893134c60a036a824367
Roman <ro.28745.maydana@gmail.com>
2019-12-05 01:06:13 -0400

    grafico3.php

--- graficos/grafico3.php
+++ graficos/grafico3.php
@@ -3,15 +3,15 @@ include('../partes/header.php');
 include('../base_datos/bd.php');
 
 
-$query = "SELECT turno, COUNT(*) as cantidad FROM personalproduccion group by turno";
+$query = "SELECT pais,SUM(nrotemporadas) as temporadas, COUNT(*) as cantidad FROM programa GROUP by pais";
 $resp = mysqli_query($conexion, $query);
 
-$query1 = "SELECT COUNT(*) as total FROM personalproduccion";
-$resp1 = mysqli_query($conexion, $query1);
-$row1 = mysqli_fetch_array($resp1);
+
+$resp1 = mysqli_query($conexion, $query);
+
 ?>
 <script src="../High/code/highcharts.js"></script>
-<script src="../High/code/highcharts-3d.js"></script>
+<!-- <script src="High/code/highcharts-3d.js"></script> -->
 <script src="../High/code/modules/exporting.js"></script>
 <script src="../High/code/modules/export-data.js"></script>
 
@@ -21,8 +21,8 @@ $row1 = mysqli_fetch_array($resp1);
             <div class="col-md-10 ">
                 <div class="card">
                     <div class="card-body">
-
-                        <div id="cont" style="height: 400px"></div>
+                    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -33,50 +33,54 @@ $row1 = mysqli_fetch_array($resp1);
 </div>
 
 <script type="text/javascript">
-    Highcharts.chart('cont', {
-        chart: {
-            type: 'pie',
-            options3d: {
-                enabled: true,
-                alpha: 45,
-                beta: 0
-            }
-        },
+    Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Número de Temporadas de un Programa según el País'
+    },
+    subtitle: {
+        text: 'Gráfico de Columnas'
+    },
+    xAxis: {
+        categories: [
+            <?php
+                while($row = mysqli_fetch_array($resp)){
+                    echo '"'.$row['pais'].'",';
+                }    
+            ?>
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
         title: {
-            text: 'Reporte de Porcentaje de Personal según el Turno.'
-        },
-        subtitle: {
-            text: <?php
-                    echo '"Total de Personal: ' . $row1['total'] . '"';
-                    ?>
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                depth: 35,
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}'
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Notas',
-            data: [
-                <?php
-                while ($row = mysqli_fetch_array($resp)) {
-                    $por = $row['cantidad'] * 100 / $row1['total'];
-                    echo '["' . strtoupper(utf8_encode($row['turno'])) . '",' . $por . '],';
-                }
-                ?>
-            ]
-        }]
-    })
+            text: 'Cantidad (unidades)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        <?php
+            while($row = mysqli_fetch_array($resp1)){
+                echo 'name:"'.utf8_encode($row['pais']).'", data: ['.$row['temporadas'].','.$row['cantidad'].']},{';
+            }    
+        ?>
+    }]
+});
 </script>
 <?php
 include('../partes/footer.php');

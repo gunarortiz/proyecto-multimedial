0f0a127360d1e62c4151b92f5741cf9efea06ed7
Gunar Ortiz <gunarortiz@gmail.com>
2019-12-05 11:33:50 -0400

    pdf1

--- reportes_pdf/pdf1.php
+++ reportes_pdf/pdf1.php
@@ -25,7 +25,7 @@ function Footer()
 }
 include('../base_datos/bd.php');
 $consulta = "SELECT p.ci, p.nombre, p.apellido, p.fecha_nac, p.telefono FROM tecnico t, persona p 
-WHERE (t.mat_trabajo LIKE 'iluminacion') AND (t.idtec LIKE p.idpersona)";
+WHERE (t.mat_trabajo LIKE 'guion') AND (t.idtec LIKE p.idpersona)";
 $resultado = $conexion->query($consulta);
 // Creación del objeto de la clase heredada
 $pdf = new PDF('L', 'mm', array(215,140));
@@ -42,7 +42,7 @@ $pdf->AddPage();
     $pdf->Ln(10);
     $pdf->Cell(80);
     // Título
-    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA ILUMINACION -',0,0,'C');
+    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DEL GUION -',0,0,'C');
     $pdf->Ln(30);
 
 $pdf->SetFont('Times','B',12);
@@ -64,7 +64,7 @@ while($row = $resultado->fetch_assoc()){
     $pdf->Cell(40, 10, $row['telefono'], 1, 1, 'C', 0);
 }
 $consulta2 = "SELECT p.ci, p.nombre, p.apellido, p.fecha_nac, p.telefono FROM tecnico t, persona p 
-WHERE (t.mat_trabajo LIKE 'vestuario') AND (t.idtec LIKE p.idpersona)";
+WHERE (t.mat_trabajo LIKE 'maquinaria') AND (t.idtec LIKE p.idpersona)";
 $resultado2 = $conexion->query($consulta2);
 // Creación del objeto de la clase heredada
 $pdf->AddPage();
@@ -79,7 +79,7 @@ $pdf->Cell(50,10,'NOMINA',0,0,'C');
 $pdf->Ln(10);
 $pdf->Cell(80);
 // Título
-$pdf->Cell(50,10,'- PERSONAL ENCARGADO DEL VESTUARIO -',0,0,'C');
+$pdf->Cell(50,10,'- PERSONAL ENCARGADO DE MAQUINARIA -',0,0,'C');
 $pdf->Ln(30);
 
 $pdf->SetFont('Times','B',12);
@@ -102,7 +102,7 @@ while($row2 = $resultado2->fetch_assoc()){
 }
 
 $consulta3 = "SELECT p.ci, p.nombre, p.apellido, p.fecha_nac, p.telefono FROM tecnico t, persona p 
-WHERE (t.mat_trabajo LIKE 'decoracion') AND (t.idtec LIKE p.idpersona)";
+WHERE (t.mat_trabajo LIKE 'operario') AND (t.idtec LIKE p.idpersona)";
 $resultado3 = $conexion->query($consulta3);
 // Creación del objeto de la clase heredada
 
@@ -118,7 +118,7 @@ $pdf->AddPage();
     $pdf->Ln(10);
     $pdf->Cell(80);
     // Título
-    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA DECORACION -',0,0,'C');
+    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA OPERACION -',0,0,'C');
     $pdf->Ln(30);
 
 $pdf->SetFont('Times','B',12);
@@ -141,7 +141,7 @@ while($row3 = $resultado3->fetch_assoc()){
 }
 
 $consulta4 = "SELECT p.ci, p.nombre, p.apellido, p.fecha_nac, p.telefono FROM tecnico t, persona p 
-WHERE (t.mat_trabajo LIKE 'color') AND (t.idtec LIKE p.idpersona)";
+WHERE (t.mat_trabajo LIKE 'escenografia') AND (t.idtec LIKE p.idpersona)";
 $resultado4 = $conexion->query($consulta4);
 // Creación del objeto de la clase heredada
 
@@ -157,7 +157,7 @@ $pdf->AddPage();
     $pdf->Ln(10);
     $pdf->Cell(80);
     // Título
-    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DEL COLOR -',0,0,'C');
+    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA ESCENOGRAFIA -',0,0,'C');
     $pdf->Ln(30);
 
 $pdf->SetFont('Times','B',12);
@@ -179,7 +179,7 @@ while($row4 = $resultado4->fetch_assoc()){
     $pdf->Cell(40, 10, $row4['telefono'], 1, 1, 'C', 0);
 }
 $consulta5 = "SELECT p.ci, p.nombre, p.apellido, p.fecha_nac, p.telefono FROM tecnico t, persona p 
-WHERE (t.mat_trabajo LIKE 'sonido') AND (t.idtec LIKE p.idpersona)";
+WHERE (t.mat_trabajo LIKE 'electricista') AND (t.idtec LIKE p.idpersona)";
 $resultado5 = $conexion->query($consulta5);
 // Creación del objeto de la clase heredada
 
@@ -195,7 +195,7 @@ $pdf->AddPage();
     $pdf->Ln(10);
     $pdf->Cell(80);
     // Título
-    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DEL SONIDO -',0,0,'C');
+    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA ELECTRICIDAD -',0,0,'C');
     $pdf->Ln(30);
 
 $pdf->SetFont('Times','B',12);
@@ -221,38 +221,5 @@ WHERE (t.mat_trabajo LIKE 'musica') AND (t.idtec LIKE p.idpersona)";
 $resultado6 = $conexion->query($consulta6);
 // Creación del objeto de la clase heredada
 
-$pdf->AddPage();
-
-    // Arial bold 15
-    $pdf->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $pdf->Cell(80);
-    // Título
-    $pdf->Cell(50,10,'NOMINA',0,0,'C');
-    // Salto de línea
-    $pdf->Ln(10);
-    $pdf->Cell(80);
-    // Título
-    $pdf->Cell(50,10,'- PERSONAL ENCARGADO DE LA MUSICA -',0,0,'C');
-    $pdf->Ln(30);
-
-$pdf->SetFont('Times','B',12);
-$pdf->SetX(10);
-
-    $pdf->Cell(30, 10, 'CI', 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, 'NOMBRE', 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, 'APELLIDO', 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, 'FECHA NAC.', 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, 'TELEFONO', 1, 1, 'C', 0);
-    $pdf->SetFont('Times','',12);
-  
-while($row6 = $resultado6->fetch_assoc()){
-    $pdf->SetX(10);
-    $pdf->Cell(30, 10, $row6['ci'], 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, $row6['nombre'], 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, $row6['apellido'], 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, $row6['fecha_nac'], 1, 0, 'C', 0);
-    $pdf->Cell(40, 10, $row6['telefono'], 1, 1, 'C', 0);
-}
 $pdf->Output();
 ?>
